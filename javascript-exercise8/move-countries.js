8495c105d4ec7f43db6ef31dc3cc0628e7104f0b
Jatin Baweja <jatin.baweja@vinsol.com>
2013-07-09 12:12:26 +0530

    Solution to JavaScript Move Country Exercise

---
+++ javascript-exercise8/move-countries.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+  //move Selected Items from one selection box to the other
+  function moveSelectedOptions(originSelection, destinationSelection){
+    var option = null;
+    for (var i=0, j=originSelection.options.length; i < j; i++) {
+      option = originSelection.options[i];
+      if (option.selected) {
+        destinationSelection.appendChild(option);
+        i--, j--;
+      }
+    }
+  }
+  //Initialize Selection Boxes
+  var countrySelection = document.getElementById("country");
+  var newCountrySelection = document.getElementById("added-countries");
+  //Initialize Add and Remove buttons
+  var addButton = document.getElementById("add-button");
+  var removeButton = document.getElementById("remove-button");
+  //Add click event listeners on add and remove buttons
+  addButton.addEventListener("click", function() {
+    moveSelectedOptions(countrySelection,newCountrySelection);
+  }, false);
+  removeButton.addEventListener("click", function() {
+    moveSelectedOptions(newCountrySelection,countrySelection);
+  }, false);
+}
