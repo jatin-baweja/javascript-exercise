5eb2937fd47129c0044d66fdafd8b02e5a57da8e
Jatin Baweja <jatin.bawej@vinsol.com>
2013-07-10 17:13:14 +0530

    Updated Solution to Javascript Input Url Exercise

--- javascript-exercise7/open-window.js
+++ javascript-exercise7/open-window.js
@@ -1,9 +1,13 @@
-var urlToOpen = prompt("Enter URL (eg. www.abc.com OR abc.com):", "");
-console.log(urlToOpen);
-if (urlToOpen != null && urlToOpen != "") {
-  urlToOpen = "http://" + urlToOpen;
-  var newWindow = window.open(urlToOpen, "_blank", "height=450,width=400,location=no,menubar=no,scrollbars=no,status=no,toolbar=no");
+function openUrl() {
+  var urlToOpen = prompt("Enter URL (www.abc.com OR abc.com):", "");
+  console.log(urlToOpen);
+  if (urlToOpen != null && urlToOpen != "") {
+    urlToOpen = "http://" + urlToOpen;
+    var newWindow = window.open(urlToOpen, "_blank", "height=450,width=400,location=no,menubar=no,scrollbars=no,status=no,toolbar=no");
+  }
+  else {
+    alert("No URL entered");
+    openUrl();
+  }
 }
-else {
-  alert("No URL entered");
-}
+openUrl();
