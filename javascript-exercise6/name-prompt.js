6c9eca600a75bc6aa837aacf340de85611f778f6
jatin-baweja <jatin.bawej@vinsol.com>
2013-07-11 15:12:46 +0530

    Updated Solution to JavaScript Name Prompt Exercise

--- javascript-exercise6/name-prompt.js
+++ javascript-exercise6/name-prompt.js
@@ -1,19 +1,19 @@
 //Function to display Prompt
-function enterName(namePosition) {
+function enterAndValidateName(namePosition) {
   var nameValue = prompt("Enter your " + namePosition + " name :", "");
   trimmedNameValue = nameValue.trim();
   //If Name is not correct, enter name again. Else, return name
   if (nameValue === null || nameValue === "" || trimmedNameValue === "" ) {
     alert("Please enter " + namePosition + " name correctly");
-    return enterName(namePosition);
+    return enterAndValidateName(namePosition);
   }
   else {
     return nameValue;
   }
 }
 //Call function
-var firstName = enterName("First");
-var lastName = enterName("Last");
+var firstName = enterAndValidateName("First");
+var lastName = enterAndValidateName("Last");
 var message = "Hello " + firstName + " " + lastName; 
 //Show alert with names
 alert(message);
