fb199bcd0b004f36842d0766eaa5808eed0f359b
jatin-baweja <jatin.bawej@vinsol.com>
2013-07-11 17:54:03 +0530

    Updated Solution to JavaScript Email and Url Exercise

--- javascript-exercise9/form-validation.js
+++ javascript-exercise9/form-validation.js
@@ -8,55 +8,68 @@ var form = {
     return label;
   }
 };
+//Patterns to check against
 var patterns = {
-  var emailPattern = /^[a-z0-9._%-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i;
-  var urlPattern = /^((http|https):\/\/)?(www.)?[\w.-]+\.[a-z]{2,4}([\/][\w%.-]+)*(\/)?([#][\w9%-]+)?([\?][\w%.]+\=[\w%]+)?(&[\w%.]+\=[\w%.]*)*$/i;
+  emailPattern : /^[a-z0-9._%-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i,
+  urlPattern : /^((http|https):\/\/)?(www.)?[\w.-]+\.[a-z]{2,4}([\/][\w%.-]+)*(\/)?([#][\w9%-]+)?([\?][\w%.]+\=[\w%]+)?(&[\w%.]+\=[\w%.]*)*$/i
 };
+//To check if a form field is empty
+function isEmptyField(field, event) {
+  var fieldName = form.getElementLabel(field);
+  if (field.value == "") {
+    alert(fieldName + " cannot be empty.");
+    field.focus();
+    event.preventDefault();
+    return true;
+  }
+  return false;
+}
+//To check if a form field is valid
+function isValidField(patternToMatch, field, event) {
+  var fieldName = form.getElementLabel(field);
+  if(!patternToMatch.test(field.value)) {
+    alert("Please enter correct " + fieldName + "!");
+    event.preventDefault();
+    return false;
+  }
+  return true;
+}
 //Form Validation class
 function FormValidation(form) {
   this.form = form;
-  this.validateForm = function(e) {
+  this.validateForm = function(event) {
     //Get all input text boxes
     var elements = form.formRoot.querySelectorAll("input[type='text'], input[type='email'], input[type='url']");
     //Check each textbox to see if it's empty or not
     for (var i = 0, j = elements.length; i < j; i++) {
-      var elementName = form.getElementLabel(elements[i]);
-      if (elements[i].value == "") {
-        alert(elementName + " cannot be empty.");
-        elements[i].focus();
-        e.preventDefault();
+      if(isEmptyField(elements[i], event)) {
         return false;
       }
     }
-    var emailFieldName = form.getElementLabel(form.emailField);
-    if(!patterns.emailPattern.test(form.emailField.value)) {
-      alert("Please enter correct " + emailFieldName + "!");
-      e.preventDefault();
+    //Check Email and HomePage Fields if they match the respective Patterns
+    if(!isValidField(patterns.emailPattern, form.emailField, event)) {
       return false;
-      }
-    var homePageFieldName = form.getElementLabel(form.homePageField);
-    if(!patterns.urlPattern.test(form.homePageField.value)) {
-      alert("Please enter correct " + homePageFieldName + "!");
-      e.preventDefault();
+    }
+    if(!isValidField(patterns.urlPattern, form.homePageField, event)) {
       return false;
-      }
+    }
     //Get textarea
     var textArea = form.formRoot.querySelector("textarea");
-    //Textarea Length
+    //Define Minimum Textarea Length
     var minLength = 50, minLengthMessage = "";
     if (textArea.value.length < minLength) {
       var textAreaName = form.getElementLabel(textArea);
       minLengthMessage = "Minimum length of " + textAreaName + " should be " + minLength + " characters.";
       alert(minLengthMessage);
       textArea.focus();
-      e.preventDefault();
+      event.preventDefault();
       return false;
     }
     //Check receive notifications
     var doOrDont = form.notificationsElement.checked ? "" : "don\'t ";
     var confirmed = confirm("Are you sure you " + doOrDont + "want to receive notifications?");
     if (!confirmed) {
-      e.preventDefault();
+      event.preventDefault();
       return false;
     }
   }
