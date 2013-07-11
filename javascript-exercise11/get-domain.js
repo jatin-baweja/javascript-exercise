dad5fddd4d79413ef64a5085ad7f95ecee2d17f4
jatin-baweja <jatin.bawej@vinsol.com>
2013-07-11 16:25:43 +0530

    Updated Solution to JavaScript Domain Extracting Exercise

--- javascript-exercise11/get-domain.js
+++ javascript-exercise11/get-domain.js
@@ -1,34 +1,36 @@
-var forms = {
-  formId : document.getElementById("url-form"),
+var form = {
+  formRoot : document.getElementById("url-form"),
   urlField : document.getElementById("url")
 };
-
-function DomainMatching(forms) {
-  this.forms = forms;
-  
+//To Store All Regex patterns
+var pattern = {
+  urlPattern : /^((http|https|ftp):\/\/)?(www.)?([\w.-]+\.[a-z]{2,4})([\/][\w%.-]+)*(\/)?([#][\w9%-]+)?([\?][\w%.]+\=[\w%]+)?(&[\w%.]+\=[\w%.]*)*$/i,
+  hostNamePattern : /^(([\w.-]+)[.])?([\w-]+\.[a-z]{2,4})$/i
+};
+//Domain Matching Class
+function DomainMatching(form) {
+  this.form = form;
   this.obtainDomain = function(e) {
-    //Url Pattern
-    var urlPattern = /^((http|https|ftp):\/\/)?(www.)?([\w.-]+\.[a-z]{2,4})([\/][\w%.-]+)*(\/)?([#][\w9%-]+)?([\?][\w%.]+\=[\w%]+)?(&[\w%.]+\=[\w%.]*)*$/i;
     //Check if pattern matches
-    if(!urlPattern.test(forms.urlField.value)) {
+    if(!pattern.urlPattern.test(form.urlField.value)) {
       alert("Please enter a URL only!");
       }
     else {
       var hostName = RegExp.$4;
-      var domainNameParts = hostName.split(".");
-      var domainName = domainNameParts.pop();
-      var domainName = domainNameParts.pop() + "." + domainName;
+      if(pattern.hostNamePattern.test(hostName)) {
+        var subDomain = RegExp.$2;
+        var domainName = RegExp.$3;
+      }
       var subDomainString = "";
-      if(domainNameParts.length > 0)
-        {
-        subDomainString = ", Subdomain : " + domainNameParts.join(".");
+      if(typeof subDomain == "string") {
+        subDomainString = ", Subdomain : " + subDomain;
         }
       alert("Domain : " + domainName + subDomainString);
       }
       e.preventDefault();
   }
   //Put a Form submit Event Listener
-  this.forms.formId.addEventListener("submit", this.obtainDomain, false);
+  this.form.formRoot.addEventListener("submit", this.obtainDomain, false);
 }
 //Initialise the objects
-var numericFormValidation = new DomainMatching(forms);
+var numericFormValidation = new DomainMatching(form);
