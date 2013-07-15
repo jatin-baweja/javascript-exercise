8da11b8985966d1533b6553406d50857118823b2
jatin-baweja <jatin.bawej@vinsol.com>
2013-07-15 14:43:55 +0530

    Updated Solution to JavaScript Numeric Validation Exercise

--- javascript-exercise10/numeric-validation.js
+++ javascript-exercise10/numeric-validation.js
@@ -1,27 +1,28 @@
-var forms = {
-  formId : document.getElementById("numeric-form"),
+var form = {
+  formRoot : document.getElementById("numeric-form"),
   numberField : document.getElementById("number"),
   resultField : document.getElementById("result")
 };
-
-function FormValidation(forms) {
-  this.forms = forms;
-  
-  this.NumericChecker = function(e) {
-    //Number Pattern
-    var numericPattern = /^\d+$/;
+//Regex Patterns
+var patterns = {
+  numericPattern : /^[\+-]?[\d]*([\.][\d]+)?$/
+};
+//Numeric Validation Class
+function NumericValidation(form) {
+  this.form = form;
+  this.validateNumber = function(event) {
     //Check if pattern matches
-    if(!numericPattern.test(forms.numberField.value)) {
-      forms.resultField.value = false;
+    if (!patterns.numericPattern.test(form.numberField.value)) {
+      form.resultField.value = false;
       alert("Please enter a numeric Value only!");
-      e.preventDefault();
-      }
+      event.preventDefault();
+    }
     else {
-      forms.resultField.value = true;
-      }
+      form.resultField.value = true;
+    }
   }
   //Put a Form submit Event Listener
-  this.forms.formId.addEventListener("submit", this.NumericChecker, false);
+  this.form.formRoot.addEventListener("submit", this.validateNumber, false);
 }
 //Initialise the objects
-var numericFormValidation = new FormValidation(forms);
+var numericFormValidation = new NumericValidation(form);
